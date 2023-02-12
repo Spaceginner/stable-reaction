de81b8bab4331e3ffe55ef79e9484f1e2acb1150
Spaceginner <ivan.demian2009@gmail.com>
2023-02-12 14:25:47 +0500

    Localization fix

--- cogs/utils/list.py
+++ cogs/utils/list.py
@@ -8,24 +8,24 @@ from modules.localization import Localization
 
 
 class List(commands.Cog):
-    command_group = discord.SlashCommandGroup("list", description=Localization(str()).get_localization('descriptions')['list']['command'])
+    command_group = discord.SlashCommandGroup("list", description=Localization(str()).get_localization('bot')['descriptions']['list']['command'])
 
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    @command_group.command(name='samplers', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('samplers'))
+    @command_group.command(name='samplers', description=Localization(str()).get_localization('bot')['descriptions']['list']['prefix'].format('samplers'))
     async def samplers(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=Localization(str()).get_localization('bot')['messages']['list_items'].format('samplers', ', '.join(Config(str(), str()).get_remote()['samplers'])))
 
-    @command_group.command(name='upscalers', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('upscalers'))
+    @command_group.command(name='upscalers', description=Localization(str()).get_localization('bot')['descriptions']['list']['prefix'].format('upscalers'))
     async def upscalers(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=Localization(str()).get_localization('bot')['messages']['list_items'].format('upscalers', ', '.join(Config(str(), str()).get_remote()['upscalers'])))
 
-    @command_group.command(name='models', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('models'))
+    @command_group.command(name='models', description=Localization(str()).get_localization('bot')['descriptions']['list']['prefix'].format('models'))
     async def models(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=Localization(str()).get_localization('bot')['messages']['list_items'].format('models', ', '.join(Config(str(), str()).get_remote()['models'])))
 
-    @command_group.command(name='embeddings', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('embeddings'))
+    @command_group.command(name='embeddings', description=Localization(str()).get_localization('bot')['descriptions']['list']['prefix'].format('embeddings'))
     async def embeddings(self, ctx: discord.ApplicationContext,
                          compatible: Option(bool, description='Show only embeddings which are compatible with the current models or all', choices=[True, False])
                          ):
@@ -34,10 +34,10 @@ class List(commands.Cog):
             embeddings_list += Config(str(), str()).get_remote()['embeddings']['skipped']
         await ctx.interaction.response.send_message(content=Localization(str()).get_localization('bot')['messages']['list_items'].format('embeddings', ', '.join(embeddings_list)))
 
-    @command_group.command(name='hypernetworks', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('hypernetworks'))
+    @command_group.command(name='hypernetworks', description=Localization(str()).get_localization('bot')['descriptions']['list']['prefix'].format('hypernetworks'))
     async def hypernetworks(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=Localization(str()).get_localization('bot')['messages']['list_items'].format('hypernetworks', ', '.join(Config(str(), str()).get_remote()['hypernetworks'])))
 
-    @command_group.command(name='styles', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('styles'))
+    @command_group.command(name='styles', description=Localization(str()).get_localization('bot')['descriptions']['list']['prefix'].format('styles'))
     async def styles(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=Localization(str()).get_localization('bot')['messages']['list_items'].format('styles', ', '.join(Config(str(), str()).get_remote()['styles'])))
