fa5003c0e5e699df28475424c810c8b7499289b1
Spaceginner <ivan.demian2009@gmail.com>
2023-02-12 14:01:37 +0500

    Placeholder subcommands for `/list` commands

--- cogs/utils/list.py
+++ cogs/utils/list.py
@@ -9,3 +9,33 @@ class List(commands.Cog):
 
     def __init__(self, bot: discord.Bot):
         self.bot = bot
+
+    @command_group.command(name='samplers')
+    async def samplers(self, ctx: discord.ApplicationContext):
+        await ctx.interaction.response.send_message(content=NotImplementedError)
+        raise NotImplementedError
+
+    @command_group.command(name='upscalers')
+    async def upscalers(self, ctx: discord.ApplicationContext):
+        await ctx.interaction.response.send_message(content=NotImplementedError)
+        raise NotImplementedError
+
+    @command_group.command(name='models')
+    async def models(self, ctx: discord.ApplicationContext):
+        await ctx.interaction.response.send_message(content=NotImplementedError)
+        raise NotImplementedError
+
+    @command_group.command(name='embeddings')
+    async def embeddings(self, ctx: discord.ApplicationContext):
+        await ctx.interaction.response.send_message(content=NotImplementedError)
+        raise NotImplementedError
+
+    @command_group.command(name='hypernetworks')
+    async def hypernetworks(self, ctx: discord.ApplicationContext):
+        await ctx.interaction.response.send_message(content=NotImplementedError)
+        raise NotImplementedError
+
+    @command_group.command(name='styles')
+    async def styles(self, ctx: discord.ApplicationContext):
+        await ctx.interaction.response.send_message(content=NotImplementedError)
+        raise NotImplementedError
