199a20eec4404d94c790767db0013757bd12f559
Spaceginner <ivan.demian2009@gmail.com>
2023-02-12 14:13:27 +0500

    Added the localization support for `/list` commands

--- cogs/utils/list.py
+++ cogs/utils/list.py
@@ -2,40 +2,41 @@ import discord
 from discord.ext import commands
 
 from modules.api.inference import API
+from modules.localization import Localization
 
 
 class List(commands.Cog):
-    command_group = discord.SlashCommandGroup("list")
+    command_group = discord.SlashCommandGroup("list", description=Localization(str()).get_localization('descriptions')['list']['command'])
 
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    @command_group.command(name='samplers')
+    @command_group.command(name='samplers', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('samplers'))
     async def samplers(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=NotImplementedError)
         raise NotImplementedError
 
-    @command_group.command(name='upscalers')
+    @command_group.command(name='upscalers', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('upscalers'))
     async def upscalers(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=NotImplementedError)
         raise NotImplementedError
 
-    @command_group.command(name='models')
+    @command_group.command(name='models', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('models'))
     async def models(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=NotImplementedError)
         raise NotImplementedError
 
-    @command_group.command(name='embeddings')
+    @command_group.command(name='embeddings', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('embeddings'))
     async def embeddings(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=NotImplementedError)
         raise NotImplementedError
 
-    @command_group.command(name='hypernetworks')
+    @command_group.command(name='hypernetworks', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('hypernetworks'))
     async def hypernetworks(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=NotImplementedError)
         raise NotImplementedError
 
-    @command_group.command(name='styles')
+    @command_group.command(name='styles', description=Localization(str()).get_localization('descriptions')['list']['prefix'].format('styles'))
     async def styles(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(content=NotImplementedError)
         raise NotImplementedError
